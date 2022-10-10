2ed199a543fbea559c8e3dd0f005538662d18bbe
Bart Grosman <grosman.bart@gmail.com>
2022-10-10 11:10:38 +0200

    Added better API handler and error handling

--- app.ts
+++ app.ts
@@ -5,27 +5,69 @@ import { Hono } from "https://deno.land/x/hono@v2.2.5/mod.ts"
 import { logger } from 'https://deno.land/x/hono@v2.2.5/middleware.ts'
 import { serveStatic } from './patched-serve-static.ts'
 
-import { apiHandler, proxyHandler } from "./handlers.ts";
+import { apiHandler, getProxyHandler } from "./handlers.ts";
 
-const app = new Hono()
+function createHonoApp(isDev: boolean, isSilent: boolean, devPort: number, localPort: number, staticFilesRoot: string) {
+  const app = new Hono()
 
-app.use('*', logger())
-app.all('/api/*', apiHandler)
+  if (!isSilent) app.use('*', logger())
+  app.all('/api/*', apiHandler)
 
-const requestHandler = Deno.env.get("DEV") ? proxyHandler : serveStatic({ root: './vueapp/dist' })
-app.use('*', requestHandler)
-app.use('*', (c, n) => {
-  console.log("Could not find the regular file in the dist folder, sending index.html")
-  return serveStatic({ path: './vueapp/dist/' })(c, n)
-})
-// If we still have not handled it, report
-app.all('*', (c) => c.text("404: Not found"))
+  const requestHandler = isDev ? getProxyHandler(devPort, localPort) : serveStatic({ root: staticFilesRoot })
+  app.use('*', requestHandler)
+  app.use('*', (c, n) => {
+    console.log("Could not find the regular file in the dist folder, sending index.html")
+    return serveStatic({ path: staticFilesRoot })(c, n)
+  })
+  // If we still have not handled it, report
+  app.all('*', (c) => c.text("404: Not found", 404))
+  
+  return app
+}
 
-const flags = parse(Deno.args, {
-  boolean: ["help", "color"],
-  string: ["version"],
-  default: { color: true },
-});
-console.log("Got flags", flags)
+if (import.meta.main) {
+  console.log("main");
+  const flags = parse(Deno.args, {
+    boolean: ["dev", "silent"],
+    string: ["dev-port", "static-files", "port", "dev-command"],
+    
+    default: {
+      dev: !!(Deno.env.get("DEV") ?? false),
+      silent: !!(Deno.env.get("SILENT") ?? false),
+      "dev-port": Deno.env.get("DEV_PORT") ?? "8080",
+      "static-files": Deno.env.get("STATIC_FILES") ?? './',
+      port: Deno.env.get("PORT") ?? '8000',
+      'dev-command': Deno.env.get("DEV_CMD") ?? ''
+    },
+  })
+  
+  console.log("Got flags", flags)
+  const port = parseInt(flags["port"])
 
-serve(app.fetch)
+  let process: Deno.Process;
+  if (flags['dev'] && flags["dev-command"]) process = Deno.run({ cmd: flags["dev-command"].split(' ') })
+
+  const app = createHonoApp(flags['dev'], flags.silent, parseInt(flags["dev-port"]), port, flags["static-files"])
+
+  serve(app.fetch, { port })
+  
+  Deno.addSignalListener("SIGINT", () => {
+    console.log("\nCaught interruption, exiting!");
+    if (process) process.kill("SIGINT")
+    Deno.exit();
+  });
+
+  addEventListener("error", (event) => {
+    console.log("Caught unhandled event:", event.message)
+    event.preventDefault()
+    if (process) {
+      const pid = process.pid
+      process.kill("SIGINT")
+      process.close()
+      console.log("done killing process", pid)
+    } else {
+      console.log("process", process)
+    }
+    Deno.exit(1);
+  })
+}
--- handlers.ts
+++ handlers.ts
@@ -1,7 +1,7 @@
-import { join, resolve, toFileUrl } from "https://deno.land/std@0.144.0/path/mod.ts";
+import { join, resolve, toFileUrl, fromFileUrl } from "https://deno.land/std@0.144.0/path/mod.ts";
 import { Context } from "https://deno.land/x/hono@v2.2.5/mod.ts"
 
-import { importModule } from 'https://deno.land/x/dynamic_import_ponyfill@v0.1.3/mod.ts'
+import { importModule } from './patched-import-module.ts'
 import { Environment, ValidatedData } from "https://deno.land/x/hono@v2.2.5/hono.ts";
 
 
@@ -28,6 +28,10 @@ export async function apiHandler(c: Context<string, Environment, ValidatedData>)
     const path = url.slice(url.indexOf('/api') + 4) || "/"
     const apiPath = APIpathResolver(path, './api')
     console.log(`Loading apiPath: ${apiPath}`)
+    
+    // First check if it exists, because importModule can throw bullshit errors
+    await Deno.stat(fromFileUrl(apiPath))
+
     const module = await importModule(apiPath)
     return module.handler(c.req)
   } catch (err) {
@@ -37,14 +41,16 @@ export async function apiHandler(c: Context<string, Environment, ValidatedData>)
   }
 }
 
-export async function proxyHandler(c: Context<string, Environment, ValidatedData>) {
-  const newUrl = c.req.url.replace(":8000", ":8080")
-  const newRequest = new Request(newUrl, c.req)
-  try {
-    const response = await fetch(newRequest)
-    return response
-  } catch(err) {
-    console.error("Failed to fetch:\n", err)
-    return c.text(err, 500)
+export function getProxyHandler(proxyPort: number, localPort: number) {
+  return async function proxyHandler(c: Context<string, Environment, ValidatedData>) {
+    const newUrl = c.req.url.replace(`:${localPort}`, `:${proxyPort}`)
+    const newRequest = new Request(newUrl, c.req)
+    try {
+      const response = await fetch(newRequest)
+      return response
+    } catch(err) {
+      console.error("Failed to fetch:\n", err)
+      return c.text(err, 500)
+    }
   }
 }
